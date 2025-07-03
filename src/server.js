bcfb059c3458af3a88d1a739b670a25a3966c4d8
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-03 22:55:54 +0530

    server

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5600;
 
 app.use(
     cors({
-    origin: "http://localhost:5173",
+    origin: "https://vibelyy.vercel.app/",
     credentials : true  // allow frontend to send the cookies
 })
 );
