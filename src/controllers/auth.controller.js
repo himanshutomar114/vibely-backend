c485f511e6ae2dd8c404d6bb888761e016d09d07
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-04 16:06:58 +0530

    auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,8 +53,8 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "none", // not prevent CSRF attacks
-      secure: true,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -85,8 +85,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "none", // not prevent CSRF attacks
-      secure: true,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(200).json({ success: true, user });
@@ -97,11 +97,7 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt", {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-  });
+  res.clearCookie("jwt");
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
