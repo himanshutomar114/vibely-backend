cf01d72f0dcb1bacc0479da7a90f189e2886fae1
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-04 15:56:53 +0530

    authcontroller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,8 +53,8 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "none", // not prevent CSRF attacks
+      secure: true,
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -85,8 +85,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "none", // not prevent CSRF attacks
+      secure: true,
     });
 
     res.status(200).json({ success: true, user });
@@ -99,8 +99,8 @@ export async function login(req, res) {
 export function logout(req, res) {
   res.clearCookie("jwt", {
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    secure: true,
   });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
